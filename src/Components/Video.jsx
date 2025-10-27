ee19551b7188015afa021240eaad5183a0399e54
Muthu Nambi <muthu142001@gmail.com>
2025-10-27 15:03:31 +0530

    fix the about and video sections

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "../Css/Video.css";
 
 const Video = () => {
-  const embedLink = "https://www.youtube.com/embed/UiixzBvHAC4";
-  const youtubeLink = "https://youtu.be/UiixzBvHAC4";
+  const youtubeLink = "https://youtu.be/s-xhkvrKSuI?si=1FUZbKVYL62-NQgs";
+  const embedLink =
+    "https://www.youtube.com/embed/s-xhkvrKSuI?si=1FUZbKVYL62-NQgs";
 
   return (
     <section className="video-section" id="video">
       <h2 className="video-title">Self Introduction</h2>
       <p className="video-description">
-        Here’s my self-introduction video — get to know me and my professional background.
+        Here’s my self-introduction video — get to know me and my professional
+        background.
       </p>
 
       <div className="video-wrapper">
